cf6169418df7403247d35b813d19d158fcd3efc4
sato-masay <sato-masay@gnavi.co.jp>
2015-09-14 17:29:21 +0900

    edit Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,29 +1,5 @@
 module.exports = function (grunt) {
 
-
-grunt.initConfig({
-  critical: {
-    dist: {
-      options: {
-        base: './'
-      },
-      // The source file
-      src: 'index.html',
-      // The destination file
-      dest: 'result.html'
-      }
-    }
-  });
-
-  // Load the plugins
-  grunt.loadNpmTasks('grunt-critical');
-
-  // Default tasks.
-  grunt.registerTask('default', ['critical']);
-};
-
-module.exports = function (grunt) {
-
 grunt.initConfig({
   critical: {
     dist: {
